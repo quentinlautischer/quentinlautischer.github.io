67179bc763b71d3a2f2302b10e96a02e93ee3f00
Quentin <qlautischer@gmail.com>
2020-08-22 14:54:33 -0600

    Updates

---
+++ precache-manifest.bbfac3146acd56faf468b469eb703f39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29f6de3dbf314171d799bb4cc6153018",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ebd851d665550af8318",
+    "url": "/static/css/main.150cb404.chunk.css"
+  },
+  {
+    "revision": "5b4c99dedc1007b66e23",
+    "url": "/static/js/2.e4521d30.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.e4521d30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ebd851d665550af8318",
+    "url": "/static/js/main.a91944bd.chunk.js"
+  },
+  {
+    "revision": "dfbf6c0c82ee5c2d6b24",
+    "url": "/static/js/runtime-main.34c7e49e.js"
+  }
+]);
